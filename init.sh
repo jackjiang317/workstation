cd4128874c651fd377b63362c2be1e2cfe7b962c
Jack Jiang <jackjiang317@gmail.com>
2019-04-23 03:44:37 -0700

    Move swapoff from init.sh to k8s roles

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 sudo apt update
 sudo apt install python2.7 python2.7-dev python-setuptools python-pip build-essential openssh-server curl apt-transport-https net-tools -y
-sudo swapoff -a
 sudo pip install --upgrade pip
 sudo pip install -I ansible==2.6
 
